e05c976b9d8b8c1816733562d11ff68b743b90e0
giovanniSant <0000110471291xsp@al.educacao.sp.gov.br>
2024-10-10 20:39:26 -0300

    js css

---
+++ sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+    createCanvas(640, 480);
+    background("orange");
+  }
+  
+  function draw() {
+    fill("blue")
+    stroke("black");
+    
+    if(mouseIsPressed){
+    rect(mouseX, mouseY, 20, 35)}
+    
+    
+    
+  
+  
+  } 
